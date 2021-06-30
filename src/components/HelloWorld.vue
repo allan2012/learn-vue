68fd5c74312edb197788f52fbc3c1106b342c301
allan <allan.koskei@gmail.com>
2021-06-30 19:20:55 +0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="hello">
+		<h1>{{ msg }}</h1>
+		<button @click="hello">Alert me</button>
+	</div>
+</template>
+
+<script>
+
+export default 
+{
+	name: "HelloWorld",
+
+	props: {
+		msg: String
+	},
+
+	methods: {
+		hello: () => {
+			alert("Hello guys its Allan")
+		},
+	}
+};
+
+</script>
+
+<style scoped>
+h1 { color: red;}
+</style>
